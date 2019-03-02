4d705a89ee00252bc86dcd2997c914fa65d4927a
elniallo <niall@hycon.io>
2019-03-02 15:41:21 +0900

    Refactor

--- src/manageCampaign.ts
+++ src/manageCampaign.ts
@@ -1,9 +1,10 @@
 import { Context,Contract } from "fabric-contract-api";
-import {Campaign} from "./campaign"
+import {Donation} from "./donation"
+import {Campaign,DonationType} from "./campaign"
 /**  Adds a project to the ledger to allow for future donations
  */
 export class ManageCampaign extends Contract {
-    async createCampaign(ctx:Context,campaignName:string): Promise<any> {
+    public async createCampaign(ctx:Context,campaignName:string): Promise<any> {
         // check if campaign exists
         let verification = await ctx.stub.getState(campaignName)
         if (verification === undefined || verification.length === 0) {
@@ -15,7 +16,7 @@ export class ManageCampaign extends Contract {
         }
         // add campaign to the ledger
     }
-    async closeCampaign(ctx:Context,campaignName:string): Promise<any>{
+    public async closeCampaign(ctx:Context,campaignName:string): Promise<any>{
         // close the campaign to donations
         let campaign_buffer = await ctx.stub.getState(campaignName)
         if (campaign_buffer === undefined || campaign_buffer.length === 0) {
@@ -26,6 +27,22 @@ export class ManageCampaign extends Contract {
             campaign.closeCampaign()
             return ctx.stub.putState(campaignName,Buffer.from(JSON.stringify(campaign)))
         } 
+    }
+
+    public async addDonation(ctx:Context,donationType:string,name:string,donorName:string,amount:number,timestamp:number): Promise<any> {
+        let campaign_buf = await ctx.stub.getState(name)
+        if (campaign_buf === undefined || campaign_buf.length === 0) {
+            throw new Error("Campaign not found")
+        } else {
+            let campaign: Campaign = JSON.parse(campaign_buf.toString())
+            let dtype:DonationType = DonationType[donationType]
+            if (dtype === undefined){
+                throw new Error("Invalid donation type")
+            }
+            let donation = new Donation(dtype,amount,name,donorName,timestamp)
+            campaign.processDonation(donation)
+            return ctx.stub.putState(campaign.projectName,Buffer.from(JSON.stringify(campaign)))
+        }
     }
 
 
